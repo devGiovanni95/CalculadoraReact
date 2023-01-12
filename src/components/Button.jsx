a703eb01bcef89610d9c90dab54b51940b262167
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-01-11 22:32:14 -0300

    Finished Layout Implements

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 import './Button.css'
 
-export default props => 
-<button className="button">{props.label}</button>
+// 1 Opção
+//export default props =>
+// <button className={`
+//     button
+//     ${props.operation ? 'operation' : '' }
+//     ${props.double ? 'double' : '' }
+//     ${props.triple ? 'triple' : '' }
+
+// `}>
+//     {props.label}
+// </button>
+
+/*2 opcao */
+export default props => {
+    let classes = 'button '
+   
+    classes += props.operation ? 'operation' : '' 
+    classes += props.double ? 'double' : '' 
+    classes += props.triple ? 'triple' : '' 
+
+    return(
+        <button
+            /**Função para pegar o valor do botão clicado */
+            onClick={e => props.click && props.click(props.label)}
+
+            className={classes}>
+            {props.label}
+        </button>
+    )
+
+}

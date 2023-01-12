5471eaca80a295cdcb19c01760aae21e771264d7
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-01-11 23:12:42 -0300

    Implements Calculator Logic

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,7 +3,19 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 import './Calculator.css'
 
+/*Criando o estado inicial da calculadora */
+const initialState = {
+    displayValue:'0',       /*Valor inicial da tela */
+    clearDisplay: false,    /**Conferir se precisa limpar o display ou não */
+    operation: null,        /**Tipo de operação que será executado */
+    values: [0,0],          /**Valores para efetuar a operacao/conta */
+    current: 0              /**Indica a posição utilizada no array values */
+}
+
 export default class Calculator extends Component{
+
+    /**Chamando o estado inicial dentro da classe*/
+    state = {...initialState}
 
     /*Criando o construtor para nao precisar referenciar novas arow functions na função render()*/
     constructor(props){
@@ -17,7 +29,8 @@ export default class Calculator extends Component{
 
     /**Funções para calculadora */
     clearMemory() {
-        console.log('Limpar')
+        // console.log('Limpar')
+        this.setState({...initialState})
     }
 
     setOperation(operation){
@@ -25,7 +38,37 @@ export default class Calculator extends Component{
     }
 
     addDigit(n){
-        console.log(n)
+        // console.log(n)
+        //Precisamos antes conferir se ja foi digitado o digito (.)ponto para que não haja mais de um instanciado 
+        
+        //validação
+        if(n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        //validação para apagar se for digitado somente 0
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+        //validação dos numeros digitado e armazenando os
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+
+        //Adicionando a troca de estado para mostrar os valores mudando
+        this.setState({
+            displayValue, clearDisplay: false
+        })
+
+        //Armazenando e convertendo o valor digitado para o array de de valores
+        //e validação se é numero ou não
+        if( n !== '.'){
+            const i = this.state.current //posicao do array
+            const newValue = parseFloat(displayValue) //convertendo texto em numero 
+            const values = [...this.state.values] //criando um novo array
+            values[i] = newValue //atribuindo os numeros digitados
+            this.setState({values})
+            console.log(values)
+        }
+
     }
 
     render(){
@@ -35,7 +78,9 @@ export default class Calculator extends Component{
 
         return(
             <div className='calculator'>
-                <Display value={100}/>
+                {/* Iniciando com o valor definido no Estado Inicial */}
+                {/* <Display value={100}/> */}
+                <Display value={this.state.displayValue}/>
 
                 {/* Usando o construtor nao precisamos passar desta forma */}
                 {/* <Button label="AC" click={() => this.clearMemory()}/> */}
